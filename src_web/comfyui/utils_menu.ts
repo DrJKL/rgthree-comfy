4defec979f6c741f6dda9400be7ff86f99c6122a
Alexander Brown <DrJKL0424@gmail.com>
2025-10-19 00:48:15 -0700

    fix: Add boolean pre-filter for the loras list
    Handles cases where the API call sometimes returns null values.

--- src_web/comfyui/utils_menu.ts
+++ src_web/comfyui/utils_menu.ts
@@ -25,7 +25,7 @@ export async function showLoraChooser(
 ) {
   const canvas = app.canvas as TLGraphCanvas;
   if (!loras) {
-    loras = ["None", ...(await rgthreeApi.getLoras().then((loras) => loras.map((l) => l.file)))];
+    loras = ["None", ...(await rgthreeApi.getLoras().then((loras) => loras.filter(Boolean).map((l) => l.file)))];
   }
   new LiteGraph.ContextMenu(loras, {
     event: event,
@@ -96,3 +96,4 @@ export function showWidgetsChooser<T extends IContextMenuValue>(
     });
   }
 }
+
